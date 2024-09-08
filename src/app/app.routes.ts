acd756cc37a4b4e1308e58e497be5e9a50717762
benmbarekalaa <alaeddine.benmbarek@adad.io>
2024-09-09 00:54:26 +0100

    progress

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'dashboard', component: RegisterComponent },
+  { path: 'dashboard', component: DashboardComponent },
 ];
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import { SideBarComponent } from '../side-bar/side-bar.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [SideBarComponent],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrl: './dashboard.component.css',
 })
 export class DashboardComponent {
+  isUploadVisible = false;
+  isUnavailableVisible = false;
+  isAccountService = true;
+  isVms = false;
 
+  showUploadInput() {
+    this.isUnavailableVisible = false;
+    this.isUploadVisible = true;
+  }
+
+  showUnavailableInput() {
+    this.isUploadVisible = false;
+    this.isUnavailableVisible = true;
+  }
+
+  showAccountService() {
+    this.isVms = false;
+    this.isAccountService = true;
+  }
+  showVMs() {
+    this.isVms = true;
+    this.isAccountService = false;
+  }
 }
