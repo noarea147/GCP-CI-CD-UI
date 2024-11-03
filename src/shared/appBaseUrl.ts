024b849f262d2c71b7cb27643b43af87773c058d
benmbarekalaa <alaeddine.benmbarek@adad.io>
2024-11-03 18:55:25 +0100

    change ui url

--- src/shared/appBaseUrl.ts
+++ src/shared/appBaseUrl.ts
@@ -1,2 +1,2 @@
-export const APP_BASE_URL = 'http://localhost:4000',
-  APP_SERVER_URL = 'http://localhost:3001';
+export const APP_BASE_URL = 'http://34.29.231.75:4000',
+  APP_SERVER_URL = 'http://34.29.231.75:3001';
