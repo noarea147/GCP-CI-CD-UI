6c8e3f4f2e4d34e5a3a13ef300cf9e9de6a7a500
benmbarekalaa <alaeddine.benmbarek@adad.io>
2024-11-13 21:53:11 +0100

    change server to domain name

--- src/shared/appBaseUrl.ts
+++ src/shared/appBaseUrl.ts
@@ -1,4 +1,4 @@
-export const APP_BASE_URL = 'http://localhost:4200',
-  APP_SERVER_URL = 'http://localhost:3001';
+export const APP_BASE_URL = 'http://skanderjerbi.fun',
+  APP_SERVER_URL = 'http://server.skanderjerbi.fun';
 // export const APP_BASE_URL = 'http://34.29.231.75:4000',
 //   APP_SERVER_URL = 'http://34.29.231.75:3001';
